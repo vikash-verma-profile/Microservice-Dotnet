8b2b1d7caffb1193e77f332709b0b7239e692b1a
Vikash-verma-profile <er.vikashverma@yahoo.com>
2024-03-12 11:34:33 +0530

    added basic layout of microsercice

---
+++ EShopping/Inventory/Controllers/LoginController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Inventory.Models;
+using Inventory.ViewModels;
+
+namespace Inventory.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly EshoppingContext _context;
+
+        public LoginController(EshoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Login
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblLogin>>> GetTblLogins()
+        {
+          if (_context.TblLogins == null)
+          {
+              return NotFound();
+          }
+            return await _context.TblLogins.ToListAsync();
+        }
+
+        // GET: api/Login/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblLogin>> GetTblLogin(int id)
+        {
+          if (_context.TblLogins == null)
+          {
+              return NotFound();
+          }
+            var tblLogin = await _context.TblLogins.FindAsync(id);
+
+            if (tblLogin == null)
+            {
+                return NotFound();
+            }
+
+            return tblLogin;
+        }
+
+        // PUT: api/Login/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTblLogin(int id, TblLogin tblLogin)
+        {
+            if (id != tblLogin.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tblLogin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblLoginExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Login
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TblLogin>> PostTblLogin(TblLogin tblLogin)
+        {
+          if (_context.TblLogins == null)
+          {
+              return Problem("Entity set 'EshoppingContext.TblLogins'  is null.");
+          }
+            _context.TblLogins.Add(tblLogin);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (TblLoginExists(tblLogin.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetTblLogin", new { id = tblLogin.Id }, tblLogin);
+        }
+
+        // DELETE: api/Login/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTblLogin(int id)
+        {
+            if (_context.TblLogins == null)
+            {
+                return NotFound();
+            }
+            var tblLogin = await _context.TblLogins.FindAsync(id);
+            if (tblLogin == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblLogins.Remove(tblLogin);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TblLoginExists(int id)
+        {
+            return (_context.TblLogins?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        [HttpPost("get-token")]
+        public async Task<IActionResult> GetToken(LoginViewModel login)
+        {
+            if (String.IsNullOrEmpty(generateToken()))
+            {
+                return Ok(generateToken());
+            }
+            return NoContent();
+        }
+
+        private string generateToken()
+        {
+            return "";
+        }
+    }
+}
---
+++ EShopping/Inventory/Program.cs
@@ -0,0 +1,34 @@
+using Inventory.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<EshoppingContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("eshoppingdbconnection")); ;
+});
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
