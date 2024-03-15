8ecf0ad17b54b999400e88c17efcc2316b911c02
Vikash-verma-profile <er.vikashverma@yahoo.com>
2024-03-15 16:21:53 +0530

    added code

---
+++ EShopping/BuildingBlocks/Common.Models/Models/EshoppingContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Common.Models.Models
+{
+    public partial class EshoppingContext : DbContext
+    {
+        public EshoppingContext()
+        {
+        }
+
+        public EshoppingContext(DbContextOptions<EshoppingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblLogin> TblLogins { get; set; } = null!;
+        public virtual DbSet<TblOrder> TblOrders { get; set; } = null!;
+        public virtual DbSet<TblProduct> TblProducts { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblLogin>(entity =>
+            {
+                entity.ToTable("tblLogin");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Password).HasMaxLength(50);
+
+                entity.Property(e => e.Username).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<TblOrder>(entity =>
+            {
+                entity.ToTable("tblOrder");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.OrderNumber).HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            });
+
+            modelBuilder.Entity<TblProduct>(entity =>
+            {
+                entity.ToTable("tblProduct");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.ProductName).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
